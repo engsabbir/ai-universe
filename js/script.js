be6fe4f46b0454b0718d43028038578b905253b9
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2023-08-31 15:05:21 +0600

    modal function start

--- js/script.js
+++ js/script.js
@@ -12,26 +12,28 @@ const displayAIData = (aiData, isShowAll) => {
     console.log(aiData.length);
     // const defaultImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png'
     const showAllBtn = document.getElementById('show-all-btn');
-    if(aiData.length > 6 && !isShowAll){
+    if (aiData.length > 6 && !isShowAll) {
         showAllBtn.classList.remove('hidden')
     }
-    else{
+    else {
         showAllBtn.classList.add('hidden')
     }
-    if(!isShowAll){
-        aiData = aiData.slice(0,6);
+    if (!isShowAll) {
+        aiData = aiData.slice(0, 6);
     }
-    
+
     aiData.forEach(data => {
-        const { image, name, published_in, features } = data;
+        const { id, image, name, published_in, features } = data;
         const div = document.createElement('div');
+        // console.log(id);
         div.innerHTML = `
-        <div class="card card-compact bg-base-100 shadow-xl p-5">
+        <div onclick="loadAIDetails('${id}')" class="card card-compact bg-base-100 shadow-xl p-5">
         <figure><img
         src="${image || `https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png`}"
         class="h-40 w-full" alt="Image not found" /></figure>
             <ul id="featured-list" class="list-decimal">
-            ${features.map(item => `<li>${item}</li>`).join('')}
+            <h3 class="text-2xl font-bold">Features:</h3>
+            ${features.map(item => `<li class="ml-5">${item}</li>`).join('')}
             </ul>
             <div class="flex justify-between mt-5 items-center">
                 <div class="grid gap-1">
@@ -46,14 +48,29 @@ const displayAIData = (aiData, isShowAll) => {
                 </div>
             </div>
         </div>
-        `;    
+        `;
         aiContainer.appendChild(div);
     })
-    
+
 }
 
+const loadAIDetails = async(id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`);
+    const data = await response.json();
+    // displayAIData(data.data);
+    // console.log(data.data);
+    
+    displayData(data.data)
+}
 
-const showAllBtn =(data)=>{
+const displayData = data => {
+    console.log(data);
+}
+// const displayAIDetails = (aiData) =>{
+//     console.log(aiData);
+// }
+
+const showAllBtn = (data) => {
     loadAIData(true)
 }
 loadAIData();
---
+++ js/sort.js
@@ -0,0 +1,5 @@
+let first = '11/1/2022';
+let second = '11/1/2022';
+let third = '11/1/2022';
+let fourth = '11/1/2022';
+let fifth = '11/1/2022';
